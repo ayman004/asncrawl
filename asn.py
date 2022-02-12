a4d3a333d5bf6378dd4322d8ffe44c7e7a520286
ayman004 <aymanxusyn@gmail.com>
2022-02-12 00:10:03 +0000

    some init msg

---
+++ asn.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+import argparse, os
+from time import sleep
+from selenium.webdriver.firefox.options import Options  
+
+firefox_options = Options()  
+firefox_options.add_argument("--headless")
+
+url= "https://bgp.he.net/"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o","--org", help="Organization to be searched for", required=True)
+args = parser.parse_args()
+org = args.org
+print("Searching for:", org)
+
+browser = webdriver.Firefox(options=firefox_options)
+browser.get("https://bgp.he.net/search?search%5Bsearch%5D="+str(org)+"&commit=Search")
+
+sleep(5)
+
+data = BeautifulSoup(browser.page_source, features="lxml")
+
+table = data.find('table',{"class":"w100p"})
+browser.quit()
+#Read table with pandas
+df = pd.read_html(table.prettify())
+df_frame = pd.DataFrame(df[0])
+df_frame.columns = ['IPs','Name']
+df_frame = df_frame[1:]
+print(df_frame)
+
+if not df_frame.empty:
+	df_frame.to_csv(str(org)+'.csv')
+if os.path.exists(str(org)+'.csv'):
+	print('Extracted IPs saved to:',os.path.abspath(str(org)+'.csv'))
